11ef73debab8e2e293461ea7f0fbc201cf79ce2c
linph <370345882@qq.com>
2018-09-25 22:57:43 +0800

    begin row and col to rudiment

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Col from './col'
+import Row from './row'
 Vue.component('d-button', Button)
 Vue.component('d-icon', Icon)
 Vue.component('d-button-group', ButtonGroup)
 Vue.component('d-input', Input)
+Vue.component('d-col', Col)
+Vue.component('d-row', Row)
 
 var app = new Vue({
   el: '#app',
