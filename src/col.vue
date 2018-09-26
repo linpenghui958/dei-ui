ba4fd0d53a906cd28df663df6fa3d558927c1877
linph <370345882@qq.com>
2018-09-26 22:56:20 +0800

    add row and col gutter

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[`col-${span}`, offset && `offset-${offset}`]"
+    :style="{paddingLeft: gutter / 2 + 'px', paddingRight: gutter / 2 + 'px'}">
+    <div style="border: 1px solid green;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 <script>
@@ -9,6 +12,14 @@
     props: {
       span: {
         type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
+      }
+    },
+    data() {
+      return {
+        gutter: 0
       }
     }
   }
@@ -17,13 +28,18 @@
   .col{
     height: 100px;
     width: 50%;
-    background-color: gainsboro;
-    border: 1px solid red;
 
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n}{
         width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
