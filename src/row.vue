c329e3a2e5172aa199dc2c3daba330d1eafa3b22
linph <370345882@qq.com>
2018-10-09 21:46:04 +0800

    add row and col align prop

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="rowStyle">
+  <div class="row" :style="rowStyle" :class="rowClass">
     <slot></slot>
   </div>
 </template>
@@ -7,7 +7,15 @@
   export default {
     name: 'DeiRow',
     props: {
-      gutter: [Number, String]
+      gutter: {
+        type: [Number, String]
+      },
+      align: {
+        type: String,
+        validator(value) {
+          return ['left', 'right', 'center'].includes(value)
+        }
+      }
     },
     mounted() {
       this.$children.forEach((vm) => {
@@ -18,6 +26,10 @@
       rowStyle() {
         let {gutter} = this
         return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+      },
+      rowClass() {
+        let {align} = this
+        return [align && `align-${align}`]
       }
     }
   }
@@ -25,5 +37,14 @@
 <style lang="scss" scoped>
   .row{
     display: flex;
+    &.align-left{
+      justify-content: flex-start;
+    }
+    &.align-right{
+      justify-content: flex-end;
+    }
+    &.align-center{
+      justify-content: center;
+    }
   }
 </style>
