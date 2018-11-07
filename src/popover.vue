2176dd0fc79212af9069517304ca27ba53f7bee3
linph <370345882@qq.com>
2018-11-07 22:44:31 +0800

    add popover basic func

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="popover" @click="xxx">
-    <slot name="content" v-if="visible"></slot>
+  <div class="popover" @click.stop="xxx">
+    <div class="content-wrapper" v-if="visible" @click.stop>
+      <slot name="content"></slot>
+    </div>
     <slot></slot>
   </div>
 </template>
@@ -16,6 +18,19 @@
     methods: {
       xxx() {
         this.visible = !this.visible
+        console.log('切换visible')
+        if (this.visible === true ) {
+            this.$nextTick(() => {
+              let eventHandler = () => {
+                this.visible = false
+                console.log('document hide popvoer')
+                document.removeEventListener('click', eventHandler)
+              }
+              document.addEventListener('click', eventHandler)
+          })
+        } else {
+          console.log('vm hide popover')
+        }
       }
     }
   }
@@ -25,5 +40,13 @@
   .popover{
     display: inline-block;
     vertical-align: top;
+    position: relative;
+    .content-wrapper{
+      position: absolute;
+      bottom: 100%;
+      left: 0;
+      border: 1px solid red;
+      box-shadow: 0 0 3px rgba(0,0,0,0.4);
+    }
   }
 </style>
