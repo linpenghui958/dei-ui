1200fca6e4570d1c19f5a0fd326b01b81d64cce6
linxiaohui <370345882@qq.com>
2018-11-19 22:44:53 +0800

    add collapse and collapse-item

---
+++ src/collapse-item.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="collapseItem">
+    <div class="title" @click="toggle" :data-name="name">
+      {{title}}
+    </div>
+    <div class="content" v-if="open">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    name: "DeiCollapseItem",
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        open: false
+      }
+    },
+    inject: ['eventBus'],
+    mounted() {
+      this.eventBus.$on('update:selected', (name) => {
+        if(name === this.name) {
+          this.open = true
+        } else {
+          this.open = false
+        }
+      })
+    },
+    methods: {
+      toggle() {
+        console.log(this.name)
+        this.eventBus.$emit('update:selected', this.name)
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  $grey: #ddd;
+  $border-radius: 4px;
+  .collapseItem {
+    > .title{
+      border: 1px solid $grey;
+      margin: -1px;
+      margin-bottom: 0px;
+      min-height: 32px;
+      display: flex;
+      align-items: center;
+      padding: 0 8px;
+      background: lighten($grey, 8%);
+    }
+    &:first-child{
+      > .title{
+        border-top-left-radius: $border-radius;border-top-right-radius: $border-radius;
+      }
+    }
+    &:last-child {
+      > .title:last-child { border-bottom-left-radius: $border-radius; border-bottom-right-radius: $border-radius; }
+    }
+    > .content { padding: 8px; }
+
+  }
+
+</style>
---
+++ src/collapse.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  import Vue from 'vue'
+  export default {
+    name: 'DeiCollapse',
+    props: {
+      selected: {
+        type: String
+      }
+    },
+    data() {
+      return {
+        eventBus: new Vue()
+      }
+    },
+    provide() {
+      return {
+        eventBus: this.eventBus
+      }
+    },
+    mounted() {
+      this.eventBus.$on('update:selected', (name) => {
+        console.log(name)
+      })
+    }
+  }
+</script>
+<style lang="scss">
+  $grey: #ddd;
+  $border-radius: 4px;
+  .collapse {
+    border: 1px solid $grey;
+    border-radius: $border-radius;
+  }
+</style>
