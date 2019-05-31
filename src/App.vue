a2c78c758f92ae29d793237d4a3e3d7c46342fe0
linpenghui958 <370345882@qq.com>
2019-05-31 23:30:49 +0800

    add table.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
-    <g-pager :totalPage="50" :current-page.sync="currentPage"></g-pager>
+    <div style="margin: 20px;">
+      <g-table :columns="columns" :data-source="dataSource"></g-table>
+    </div>
+    <div style="margin: 20px;">
+      <g-pager :totalPage="50" :current-page.sync="currentPage"></g-pager>
+    </div>
   </div>
 </template>
 
 <script>
 import GPager from '@/pager/pager'
+import GTable from '@/table/table'
 export default {
   name: "app",
   components: {
-    GPager
+    GPager,
+    GTable
   },
   data() {
     return {
-      currentPage: 1
+      currentPage: 1,
+      columns: [
+        { text: '姓名', field: 'name'},
+        { text: '分数', field: 'score'},
+      ],
+      dataSource: [
+        { id: 1, name: 'linph', score: 100},
+        { id: 2, name: 'lee', score: 60},
+      ]
     };
   },
 };
---
+++ src/table/table.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th v-for="(column, index) in columns" :key="index">{{column.text}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in dataSource" :key="index">
+          <td>{{index + 1}}</td>
+          <template v-for="(column, index) in columns">
+            <td :key="index">{{item[column.field]}}</td>
+          </template>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    columns: {
+      type: Array,
+      required: true
+    },
+    dataSource: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
